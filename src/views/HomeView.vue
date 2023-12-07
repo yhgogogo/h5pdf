1b912ffdfa03c13c7fd4ffd571b7b443a3d4d9f6
yh-199956 <44158296@qq.com>
2023-12-07 13:04:19 +0800

    变更

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <a href="/assets/pdfs/1.pdf" target="_blank">pdf 查看</a>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import QRCode from 'qrcode'
+export default {
+  name: 'HomeView',
+  data(){
+    return{
+      src:'/',
+
+    }
+  },
+  mounted(){
+    this.makeQr()
+  },
+  methods:{
+    makeQr(){
+      console.log(this.src);
+      QRCode.toDataURL(this.src)
+      .then(url => {
+        console.log(url)
+        this.pdfurl=url
+      })
+      .catch(err => {
+        console.error(err)
+      })
+
+    }
+  }
+}
+</script>
